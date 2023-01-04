c2baa70c05c0d8406ad90aaa8f57e1b0280b398d
Yandry Almaguer <yandryad82@gmail.com>
2023-01-04 11:31:08 -0300

    Nombre Pais

--- src/components/WeatherToday.jsx
+++ src/components/WeatherToday.jsx
@@ -5,32 +5,19 @@ import { useState } from 'react';
 import axios from 'axios';
 import { format } from 'date-fns';
 import imgSol from '../images/sol.png'
+import countriesName from '../countriesName.json'
 
 const WeatherToday = () => {
   
   //Todo el código JS se escribe aquí
-
-  // Detectando Posicion
- // const [latitude, setLatitude] = useState(null);
- // const [longitude, setLongitude] = useState(null);
-
- // useEffect(() => {
- //   navigator.geolocation.getCurrentPosition((position) => {
- //     setLatitude(position.coords.latitude);
- //     setLongitude(position.coords.longitude);
- //   });
- // }, []);
-  
-  //   
-   
-  
+  console.log(countriesName)
   const [data, setData] = useState({})
      
   useEffect(() => {
     function success(pos) {
         const crd = pos.coords;
         axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${crd.latitude}&lon=${crd.longitude}&appid=4bd21af8999321574db120507ed62a45`)
-            .then(res => setData(res.data))
+        .then(res => setData(res.data))
         console.log('Your current position is:');
         console.log(`Latitude : ${crd.latitude}`);
         console.log(`Longitude: ${crd.longitude}`);
@@ -41,7 +28,6 @@ const WeatherToday = () => {
     navigator.geolocation.getCurrentPosition(success, error);
 }, [])
   //
-
    
   const tempC = ((data.main?.temp)-273.15).toFixed(2);
 
@@ -52,6 +38,12 @@ const WeatherToday = () => {
   const tempctotempf = () => {
     setChangeTemp(!changeTemp);
   }
+
+  //Nombre Country
+  let currentCountry = countriesName.find(function (country) {
+    return country.code === `${data.sys?.country}`
+})
+  //
      
   const imgNub = "https://static.vecteezy.com/system/resources/previews/012/806/414/original/3d-cartoon-weather-rain-clouds-with-thunderstorm-dark-cloud-sign-with-lightning-isolated-on-transparent-background-3d-render-illustration-png.png"
 
@@ -71,7 +63,7 @@ const WeatherToday = () => {
     <div className='general-container-app'>
       <div className='container-title'>
         <h1>Weather App</h1>
-        <h3>{data.name} {data.sys?.country}</h3>
+        <h3>{data.name} {currentCountry?.name}</h3>
       </div>
       <div className='container-date'>
         <span>{format(new Date(), "PPP")}</span>
